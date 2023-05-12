ed67371418f142f207b059e166fb9ec8a55521de
alonabrytvina <alona.brytvina@nixs.com>
2023-05-12 17:44:42 +0200

    hide mongo psw, set up ConfigModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductsModule } from './products/products.module';
+import * as process from 'process';
 
 // концепт - декораторы, экспортируем класс, который украшаем декоратором.
 // Тут используем декоратор @Module
@@ -11,8 +13,11 @@ import { ProductsModule } from './products/products.module';
 @Module({
     imports: [
         ProductsModule,
+        ConfigModule.forRoot({
+            envFilePath: '.development.env',
+        }),
         MongooseModule.forRoot(
-            `mongodb+srv://Alona:Eg6J0HmJtfWcoc4N@cluster0.dhqdfve.mongodb.net/?retryWrites=true&w=majority`
+            `mongodb+srv://Alona:${process.env.MONGO_PSW}@cluster0.dhqdfve.mongodb.net/?retryWrites=true&w=majority`
         )
     ],
     controllers: [AppController],
